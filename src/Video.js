c931e18b61a749bc202499de46aa1dbfcd091cf3
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-10-28 16:32:55 -0700

    create video component

---
+++ src/Video.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <video 
+        src="https://www.tiktok.com/@tdog_kennels/video/6880991966050503938?lang=en"
+      >
+      </video>
+
+      {/* Video Footer */}
+      {/* Video Sidebar*/}
+    </div>
+  )
+}
+
+export default Video
