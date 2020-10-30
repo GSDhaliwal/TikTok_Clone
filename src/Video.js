75258ea43c1747c80380bae5db0feb467f7baaf3
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-10-30 09:41:28 -0700

    create video footer

--- src/Video.js
+++ src/Video.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from 'react'
 import "./Video.css";
+import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
 function Video() {
-  const [playing, setPlaying] = useState(false)
   
+  const [playing, setPlaying] = useState(false)
   const videoRef = useRef(null);
-
   const handleVideoPress = () => {
     if (playing) {
       videoRef.current.pause();
@@ -26,7 +27,8 @@ function Video() {
       src="https://player.vimeo.com/external/426673801.sd.mp4?s=5ae53c0adb71bc9f2098e8a8f6a18fa3f27337c9&profile_id=165&oauth2_token_id=57447761"></video>
 
     
-
+      <VideoFooter />
+      <VideoSidebar />
       {/* Video Footer */}
       {/* Video*/}
     </div>
---
+++ src/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./VideoFooter.css"
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter_text">
+        <h3>@channid86</h3>
+        <p>Random description</p>  
+      </div>
+    </div>
+  )
+}
+
+export default VideoFooter
