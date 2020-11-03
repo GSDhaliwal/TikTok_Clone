f18bc5c5c75e631a2139a8c242e9281792f3757d
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-11-03 15:48:27 -0800

    add material ui icons and music note to footer

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./VideoFooter.css"
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
 
 function VideoFooter() {
   return (
     <div className="videoFooter">
       <div className="videoFooter_text">
         <h3>@channid86</h3>
-        <p>Random description</p>  
+        <p>Random description</p> 
+        <div className="videoFooter_ticker">
+          <MusicNoteIcon className="videoFooter_icon" />
+        </div> 
       </div>
     </div>
   )
